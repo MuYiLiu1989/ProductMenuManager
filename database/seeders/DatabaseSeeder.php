71f7c6c059186f636bf56015798a63093d7aae84
MuYiYang <chenmy1989@gmail.com>
2025-09-02 19:12:47 +0800

    讓ProductItemSeeder可以運作

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ProductCategorySeeder::class,
-            //ProductItemSeeder::class,
+            ProductItemSeeder::class,
         ]);
     }
 }
